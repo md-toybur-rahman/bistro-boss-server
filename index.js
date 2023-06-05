8c88b51c95443e7f78587aab32f65dc0528526b4
md-toybur-rahman <toyburrahman735@gmail.com>
2023-06-05 11:08:53 +0600

    jwt and adimn middleware

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// jwt middleware
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization) {
+        return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(401).send({error: true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 
 
@@ -40,9 +57,28 @@ async function run() {
             res.send({token})
         })
 
+        // warning: use verifyJWT before using verifyAdmin
+        const verifyAdmin = async(req, res, next) => {
+            const email = req.decoded.email;
+            const query = {email: email}
+            const user = await usersCollection.findOne(query);
+            if(user?.role !== 'admin') {
+                return res.status(403).send({error: true, message: 'forbidden access'});
+            }
+            next();
+        }
+
+        /**
+         * 1. do not show secure links to those who should not see the links 
+         * 2. use JWT token: verifyJWT
+         * 3. use verifyAdmin middleware
+         * 
+         * **/ 
+
+
         // users related apis
 
-        app.get('/users', async(req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async(req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result)
         })
@@ -57,6 +93,21 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
+
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if(req.decoded.email !== email) {
+                res.send({admin: false})
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { admin: user?.role === 'admin'}
+            res.send(result);
+        })
+
 
         app.patch('/users/admin/:id', async(req, res) => {
             const id = req.params.id;
@@ -83,10 +134,14 @@ async function run() {
 
         // cart
 
-        app.get('/carts', async(req, res) => {
+        app.get('/carts', verifyJWT, async(req, res) => {
             const email = req.query.email;
             if(!email){
                 res.send([])
+            }
+            const decodedEmail = req.decoded.email;
+            if(email !== decodedEmail) {
+                return res.status(403).send({error: true, message: 'Forbidden Access'})
             }
             const query = {email: email};
             const result = await cartCollection.find(query).toArray();
